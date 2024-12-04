2148878ead7158accd97eec86ea5d5d2e651a249
Maksim <maksim_maxim@live.com>
2024-12-04 10:41:33 -0800

    Test secrets

--- functions/ngrok_test.protected.js
+++ functions/ngrok_test.protected.js
@@ -2,7 +2,7 @@
 exports.handler = function(context, event, callback) {
   
     let twiml = new Twilio.twiml.VoiceResponse();
-    twiml.say('Ngrok works! 23 deploy test')
+    twiml.say('Ngrok works! 23 deploy test secrets added')
   
     return callback(null, twiml);
   };
