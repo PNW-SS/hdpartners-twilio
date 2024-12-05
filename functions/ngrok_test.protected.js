582e3da39c84697e9926edaab95b39aa9cb7a739
Maksim <maksim_maxim@live.com>
2024-12-05 09:52:39 -0800

    remove ngork test

--- functions/ngrok_test.protected.js
+++
@@ -1,8 +0,0 @@
-
-exports.handler = function(context, event, callback) {
-  
-    let twiml = new Twilio.twiml.VoiceResponse();
-    twiml.say('Test dev push keys test env remove env');
-  
-    return callback(null, twiml);
-  };
