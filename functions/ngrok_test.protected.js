3a59ac3c0187b87d8f838d4fbdf84ad79caf0a9d
Maksim <maksim_maxim@live.com>
2024-12-05 08:38:18 -0800

    test env

--- functions/ngrok_test.protected.js
+++ functions/ngrok_test.protected.js
@@ -2,7 +2,7 @@
 exports.handler = function(context, event, callback) {
   
     let twiml = new Twilio.twiml.VoiceResponse();
-    twiml.say('Test dev push keys');
+    twiml.say('Test dev push keys test env');
   
     return callback(null, twiml);
   };
